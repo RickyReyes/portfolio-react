bf61cb1776be71a8c4093f857883997d7dbf86d1
RickyReyes <77644172+RickyReyes@users.noreply.github.com>
2023-02-16 17:14:28 -0800

    audiophile added

--- src/App.js
+++ src/App.js
@@ -12,22 +12,33 @@ function App() {
       photoQuery: "cocktails",
       techStack: ["React", "Tailwind CSS"],
       description:
-        "An app about cocktails for those of legal drinking age. Browse through cocktails, or select your available ingredients and see what cocktails you can make.",
+        "My baby! Browse through cocktails, or select your available ingredients and see what cocktails you can make. Includes a simple algorithm for sorting cocktails based on selected ingredients, and memoization, routing, filters.",
       github: "https://github.com/RickyReyes/cocktails",
       liveSite: "https://cocktails-nu-nine.vercel.app/",
     },
     {
       id: 2,
+      name: "Audiophile",
+      photoQuery: "audiophile",
+      techStack: ["React", "React Router", "Sass"],
+      description:
+        "A fully responsive, multi-page website for a mock e-commerce electronics company, with input validation, a cart modal, and routing.",
+      github: "https://github.com/RickyReyes/audiophile",
+      liveSite: "https://audiophile-pink.vercel.app/",
+    },
+    {
+      id: 3,
       name: "Flag Game",
       photoQuery: "flag-game",
       techStack: ["React", "Tailwind CSS", "External API"],
       description:
-        "A short quiz for testing your international flag knowledge. Pulls data from the Rest Countries API.",
+        "My first creative project! A short quiz that tests your international flag knowledge. Pulls data from the REST Countries API. Can you score 10/10?",
       github: "https://github.com/RickyReyes/flag-game-react",
       liveSite: "https://flag-game-react-nine.vercel.app",
     },
+
     {
-      id: 3,
+      id: 4,
       name: "To-do List",
       photoQuery: "todo-list",
       techStack: ["React", "React Hooks"],
@@ -37,17 +48,18 @@ function App() {
       liveSite: "https://todo-app-pink-sigma.vercel.app/",
     },
     {
-      id: 4,
+      id: 5,
       name: "DevJobs",
       photoQuery: "devjobs",
-      techStack: ["TypeScript", "React", "React Router DOM"],
+      techStack: ["TypeScript", "React", "React Router"],
       description:
         "A mock website for a tech job postings site. Includes a modal for small screen sizes, filter functionality and routing for each job's detail page.",
       github: "https://github.com/RickyReyes/devjobs",
       liveSite: "https://devjobs-seven.vercel.app/",
     },
+
     {
-      id: 5,
+      id: 6,
       name: "Comments",
       photoQuery: "comments",
       techStack: ["TypeScript", "React", "OOP"],
@@ -55,19 +67,9 @@ function App() {
       github: "https://github.com/RickyReyes/comments-ts",
       liveSite: "https://comments-ts.vercel.app/",
     },
-    {
-      id: 6,
-      name: "Bookmark Landing Page",
-      photoQuery: "bookmark",
-      techStack: ["Flexbox", "Vanilla JS"],
-      description: "A responsive landing page for a mock company.",
-      github: "https://github.com/RickyReyes/bookmark",
-      liveSite: "https://bookmark-dun-pi.vercel.app/",
-    },
   ];
 
   const [showMore, setShowMore] = useState(false);
-
   let renderedProjects = showMore ? projectsData : projectsData.slice(0, 3);
 
   return (
